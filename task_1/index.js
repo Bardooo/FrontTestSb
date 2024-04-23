34a3c7cf7fb34e8da9e75477552999d0b25d6407
Deni <00amfibinator00@mail.ru>
2024-04-23 19:57:16 +0300

    first task

--- task_1/index.js
+++ task_1/index.js
@@ -3,6 +3,24 @@ import {encoded, translations} from './data.js'
 console.log("Let's rock")
 console.log(encoded, translations)
 
+function decodedData(encoded) {
+  const decoded = [];
 
+  for (const dataEl of encoded) {
+    const res = {};
+    for (const el in dataEl) {
+      if (el.includes('Id') && el !== 'groupId' && el !== "service" && el !== "formatSize" && el !== "ca" && dataEl[el]) {
+        res[el] = translations[dataEl[el]];
+      } else {
+        res[el] = dataEl[el];
+      }
+    }
 
-// console.log(decoded)
+    decoded.push(res);
+  } 
+
+  return decoded;
+}
+const decoded = decodedData(encoded);
+
+console.log(decoded)
